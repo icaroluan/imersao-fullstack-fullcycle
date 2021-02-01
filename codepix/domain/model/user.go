de671418bc25cbb10be460081a164005fd7b76a4
Icaro Luan Rodrigues Freitas <icaro.freitas@civil.com.br>
2021-02-01 20:09:55 -0300

    desafio1

---
+++ codepix/domain/model/user.go
@@ -0,0 +1,40 @@
+package model 
+
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+type User struct {
+	ID        string	`json:"id" valid:"uuid"`
+	Name      string	 `json:"name" valid:"notnull"`
+	Email     string	 `json:"email" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewUser(id string, name string, Email string) (*User, error) {
+	user := User{
+		ID: id,
+		Name: name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt = time.Now()
+
+	err := user.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
